82f0c8968a04a18b879aaa6e3891bbd275cd61bf
Duarte Brito <duarte.brito@findmore.eu>
2024-09-27 02:10:18 +0100

    Refactor logger to stringify argument values in Discord logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -49,7 +49,7 @@ async function logDiscord(
     const fields = Object.keys(args).map((key) => {
       return {
         name: key,
-        value: args[key].toString(),
+        value: JSON.stringify(args[key]),
         inline: true,
       };
     });
